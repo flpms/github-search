89547a3a7d96f6db4da43b42d11221bc7ecbf1b5
Filipe M. Silva <filipe@smartrips.co>
2018-07-06 03:27:12 -0300

    add components to main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SearchComponent } from './components/search/search.component';
+import { SearchBarComponent } from './components/shared/search-bar/search-bar.component';
+import { StarsComponent } from './components/shared/stars/stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NotFoundComponent,
+    ProfileComponent,
+    SearchBarComponent,
+    SearchComponent,
+    StarsComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
