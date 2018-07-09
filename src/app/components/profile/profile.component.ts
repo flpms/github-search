e9694f707842b8911556b2d854debb98b0159afd
Filipe M. Silva <filipe@smartrips.co>
2018-07-09 16:47:41 -0300

    create API consult for user profile and repos

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { UsersService } from './../../services/users.service';
 
 @Component({
   selector: 'gs-profile',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public user:any;
+  public repos:any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private usersService: UsersService
+  ) { }
 
   ngOnInit() {
+    let name = this.route.snapshot.paramMap.get('username');
+    this.getUser(name);
   }
 
+  private getUser(name:string):void {
+    this.usersService
+      .getUser(name)
+      .subscribe((result:any) => {
+        if (!result) {
+          return this.router.navigate(['not-found/', name]);
+        }
+
+        this.user = result;
+        this.getUserRepos(name);
+      });
+  }
+
+  private getUserRepos(username:string):void {
+    this.usersService
+        .getRepos(username)
+        .subscribe((result) => {
+          this.repos = result;
+          this.calcStars();
+        });
+  }
+
+  private calcStars():void {
+    this.user.stars = this.repos.reduce((acc, value) => acc + value.stargazers_count, 1);
+  }
 }
