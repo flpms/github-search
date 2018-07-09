1abc458a535b2db1dcb3ef615f9a1ed31fa001c6
Filipe M. Silva <filipe@smartrips.co>
2018-07-09 16:47:01 -0300

    create action for logo click

--- src/app/components/search-header/search-header.component.ts
+++ src/app/components/search-header/search-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'gs-search-header',
@@ -8,14 +8,28 @@ import { Router } from '@angular/router';
 })
 export class SearchHeaderComponent implements OnInit {
 
+  public isSearchLayout: boolean = true;
+
   constructor(
-    private router:Router
+    private router:Router,
+    private route: ActivatedRoute,
   ) { }
 
   ngOnInit() {
+    let { snapshot } = this.route;
+    this.isSearchLayout = this.checkRouteConfig(snapshot);
+  }
+
+  private checkRouteConfig(snapshot):boolean {
+    let { path } = snapshot.routeConfig;
+    return (path === 'search');
   }
 
   public search(query:any):void {
     this.router.navigate(['profile/', query]);
+  }
+
+  public goBack():void {
+    !this.isSearchLayout && this.router.navigate(['search/']);
   }
 }
