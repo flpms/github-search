1ba92222851ea08cdf2cefb6193dec0be2838ee5
Filipe M. Silva <filipe@smartrips.co>
2018-07-09 13:21:21 -0300

    add new components and remove some modules from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SearchHeaderComponent } from './components/search-header/search-header.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { SearchComponent } from './components/search/search.component';
 import { SearchBarComponent } from './components/shared/search-bar/search-bar.component';
 import { StarsComponent } from './components/shared/stars/stars.component';
+import { LogoComponent } from './components/shared/logo/logo.component';
+import { ProfileDetailComponent } from './components/shared/profile-detail/profile-detail.component';
 
 import { UsersService } from './services/users.service';
 
@@ -17,12 +18,13 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     NotFoundComponent,
     ProfileComponent,
     SearchBarComponent,
-    SearchComponent,
-    StarsComponent
+    SearchHeaderComponent,
+    StarsComponent,
+    ProfileDetailComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
