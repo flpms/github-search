fa73ddbcb2a3332513a7d7b1f3064126b3f78fb0
Filipe M. Silva <filipe@smartrips.co>
2018-07-09 23:17:12 -0300

    remove force ssl

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const app = express();
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-
-app.use(forceSSL);
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
